faeb57bff390b431702b5c1618dcebab960783f5
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2025-09-24 08:41:40 +0500

    update

--- book-service/src/main/java/uz/pdp/book_service/BookServiceApplication.java
+++ book-service/src/main/java/uz/pdp/book_service/BookServiceApplication.java
@@ -2,14 +2,16 @@ package uz.pdp.book_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
 
 }
