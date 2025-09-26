4f00f3ab59da8ecaadcd8c0ad87be0912e9df36a
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2025-09-26 09:18:12 +0500

    fix

--- book-service/src/main/java/uz/pdp/book_service/repository/BookRepository.java
+++ book-service/src/main/java/uz/pdp/book_service/repository/BookRepository.java
@@ -3,9 +3,11 @@ package uz.pdp.book_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uz.pdp.book_service.entity.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Transactional
     @Modifying
--- student-service/src/main/java/uz/pdp/student_service/repository/StudentRepository.java
+++ student-service/src/main/java/uz/pdp/student_service/repository/StudentRepository.java
@@ -2,10 +2,12 @@ package uz.pdp.student_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import uz.pdp.student_service.entity.Student;
 
 import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query(value = "select s.id from student s", nativeQuery = true)
     List<Integer> getAllStudentIda();
